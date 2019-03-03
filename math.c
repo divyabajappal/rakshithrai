a1cfb3e510f562d2ed853559d30f8802c266f401
rakshithrai <rakshithrai90@gmail.com>
2019-03-03 12:21:23 +0530

    hello

---
+++ math.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//this a modified file
+void sin()
+{
+print("we are calling sin function");
+}
+void cos()
+{
+print("we are calling cos function");
+}
+void tan()
+{
+print("we are calling tan function");
+}
+int main()
+{
+sin();
+cos();
+tan();
+}
