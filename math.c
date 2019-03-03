d2486e2b03350efe63d690811a81d4499e5d00cc
rakshithrai <rakshithrai90@gmail.com>
2019-03-03 12:41:07 +0530

    Added cos function

--- math.c
+++ math.c
@@ -4,17 +4,9 @@ void sin()
 {
 print("we are calling sin function");
 }
-void cos()
-{
-print("we are calling cos function");
-}
-void tan()
-{
-print("we are calling tan function");
-}
+
 int main()
 {
 sin();
-cos();
-tan();
+
 }
